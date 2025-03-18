116cd7382567f3abca1612ddd0967ceeb8149239
yanxiao723 <2152683116@qq.com>
2025-03-18 15:18:34 +0800

    follow ps1 step

--- ps0/ps0/src/turtle/TurtleSoup.java
+++ ps0/ps0/src/turtle/TurtleSoup.java
@@ -15,7 +15,11 @@ public class TurtleSoup {
      * @param sideLength length of each side
      */
     public static void drawSquare(Turtle turtle, int sideLength) {
-        throw new RuntimeException("implement me!");
+        for(int i=0;i<4;i++){
+        turtle.forward(sideLength);
+        turtle.turn(90.0);
+        // throw new RuntimeException("implement me!");
+        }
     }
 
     /**
